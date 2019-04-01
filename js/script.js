55192f952a9171f5c1a2d1cf8aaaa349146a68ce
zubeir-Abubakar <zubkayare@gmail.com>
2019-04-01 14:37:46 +0300

    function call test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
- function check(b){
+ function check(){
 
 var question1 = document.quiz.question1.value;
 var question2 = document.quiz.question2.value;
 var question3 = document.quiz.question3.value;
 var question4 = document.quiz.question4.value;
-var question5 = document.quiz.question5.value;
+var question5 = document.qui z.question5.value;
 var question6 = document.quiz.question6.value;
 var question7 = document.quiz.question7.value;
 var question8 = document.quiz.question8.value;
